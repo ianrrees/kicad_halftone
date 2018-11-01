bf1927d97074f7faec0c00f1e2c8dae32dd384f6
Ian Rees <ian.rees@gmail.com>
2018-11-01 20:46:29 +1300

    Using modules!

---
+++ src/gui/mod.rs
@@ -0,0 +1,57 @@
+// mod gui;
+
+extern crate iui;
+use self::iui::prelude::*;
+use self::iui::controls::{HorizontalBox, VerticalBox, Button};
+
+pub fn launch_gui() {
+    let ui = UI::init().unwrap();
+
+    let mut window = Window::new(&ui, "Halftone generator", 640, 480, WindowType::NoMenubar);
+    let mut hbox = HorizontalBox::new(&ui);
+
+    let mut open_button = Button::new(&ui, "Open Button!");
+    let mut save_button = Button::new(&ui, "Save Button!");
+    // let mut previewArea = Area::new(&ui);
+
+    open_button.on_clicked(&ui, |_| {
+        println!("open_button got clicked!");
+        if let Some(path) = &window.open_file(&ui) {
+            println!("Would open {:?}", &path);
+            // let mut file = match File::create(&path) {
+            //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not open file {}: {}", path.display(), why.description())); return; }
+            //     Ok(f) => f
+            // };
+            // match file.write_all(entry.value(&ui).as_bytes()) {
+            //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not write to file {}: {}", path.display(), why.description())); return; }
+            //     Ok(_) => ()
+            // };
+        }
+    });
+
+    hbox.append(&ui, open_button, LayoutStrategy::Stretchy);
+    hbox.append(&ui, save_button.clone(), LayoutStrategy::Stretchy);
+    // hbox.append(&ui, previewArea.clone(), LayoutStrategy::Stretchy);
+
+    window.set_child(&ui, hbox);
+    window.show(&ui);
+
+    // save_button.on_clicked(&ui, {
+    //     println!("save_button got clicked!");
+    //     let ui = ui.clone();
+    //     move |_| {
+    //         if let Some(path) = window.save_file(&ui) {
+    //             // let mut file = match File::create(&path) {
+    //             //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not open file {}: {}", path.display(), why.description())); return; }
+    //             //     Ok(f) => f
+    //             // };
+    //             // match file.write_all(entry.value(&ui).as_bytes()) {
+    //             //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not write to file {}: {}", path.display(), why.description())); return; }
+    //             //     Ok(_) => ()
+    //             // };
+    //         }    
+    //     }
+    // });
+
+    ui.main();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 extern crate image;
 
 #[cfg(feature="gui")]
-extern crate iui;
-#[cfg(feature="gui")]
-use iui::prelude::*;
-#[cfg(feature="gui")]
-use iui::controls::{HorizontalBox, VerticalBox, Button};
+pub mod gui;
 
 // use std::fmt;
 use std::fs::File;
@@ -33,59 +29,6 @@ fn parse_cli_args(args: Vec<String>) -> Result<(String, String), &'static str> {
     Ok((source, dest))
 }
 
-#[cfg(feature="gui")]
-fn launch_gui() {
-    let ui = UI::init().unwrap();
-
-    let mut window = Window::new(&ui, "Halftone generator", 640, 480, WindowType::NoMenubar);
-    let mut hbox = HorizontalBox::new(&ui);
-
-    let mut open_button = Button::new(&ui, "Open Button!");
-    let mut save_button = Button::new(&ui, "Save Button!");
-    // let mut previewArea = Area::new(&ui);
-
-    open_button.on_clicked(&ui, |_| {
-        println!("open_button got clicked!");
-        if let Some(path) = &window.open_file(&ui) {
-            println!("Would open {:?}", &path);
-            // let mut file = match File::create(&path) {
-            //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not open file {}: {}", path.display(), why.description())); return; }
-            //     Ok(f) => f
-            // };
-            // match file.write_all(entry.value(&ui).as_bytes()) {
-            //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not write to file {}: {}", path.display(), why.description())); return; }
-            //     Ok(_) => ()
-            // };
-        }
-    });
-
-    hbox.append(&ui, open_button, LayoutStrategy::Stretchy);
-    hbox.append(&ui, save_button.clone(), LayoutStrategy::Stretchy);
-    // hbox.append(&ui, previewArea.clone(), LayoutStrategy::Stretchy);
-
-    window.set_child(&ui, hbox);
-    window.show(&ui);
-
-    // save_button.on_clicked(&ui, {
-    //     println!("save_button got clicked!");
-    //     let ui = ui.clone();
-    //     move |_| {
-    //         if let Some(path) = window.save_file(&ui) {
-    //             // let mut file = match File::create(&path) {
-    //             //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not open file {}: {}", path.display(), why.description())); return; }
-    //             //     Ok(f) => f
-    //             // };
-    //             // match file.write_all(entry.value(&ui).as_bytes()) {
-    //             //     Err(why) => { window.modal_err(&ui, "I/O Error", &format!("Could not write to file {}: {}", path.display(), why.description())); return; }
-    //             //     Ok(_) => ()
-    //             // };
-    //         }    
-    //     }
-    // });
-
-    ui.main();
-}
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("Got {} args: {:?}", args.len(), args);
@@ -93,7 +36,7 @@ fn main() {
     if args.len() == 1 {
         #[cfg(feature="gui")] {
             println!("Start GUI version");
-            launch_gui();
+            gui::launch_gui();
         }
         #[cfg(not(feature="gui"))] {
             println!("GUI not built in, sorry...");
